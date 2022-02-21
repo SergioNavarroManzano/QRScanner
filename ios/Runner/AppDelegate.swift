638c5c87e315887d44c7e18e2c0d97e9ca5672ab
Sergio Navarro Manzano <sergionavarroman@gmail.com>
2022-02-21 17:56:00 +0100

    Subida para paso a mac

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDvp41c8edubetR42hovHO5tmFy4zrptfs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,20 +1,40 @@
+import 'dart:async';
+
 import 'package:escaner_sql/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+
+  Completer<GoogleMapController> _controller = Completer(); 
 
   @override
   Widget build(BuildContext context) {
 
+    final CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
     final ScanModel scan=ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(
-        child: Text(scan.valor),
-     ),
+     body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
    );
   }
 }
